b0f9769d60ba1b240a94cfeb491972749fac0441
Tom LanFranca <ittechtommy@gmail.com>
2018-02-27 09:14:24 -0600

    added docs folder for github pages

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,13 +11,13 @@ gulp.task("previewDist", function() {
   browserSync.init({
     notify: false,
     server: {
-      baseDir: "dist"
+      baseDir: "docs"
     }
   });
 });
 
 gulp.task("deleteDistFolder", function() {
-  return del("./dist");
+  return del("./docs");
 });
 
 gulp.task("optimizeImages", ["deleteDistFolder"], function() {
@@ -27,7 +27,7 @@ gulp.task("optimizeImages", ["deleteDistFolder"], function() {
       interlaced: true,
       multipass: true
     }))
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest("./docs/assets/images"));
 });
 
 gulp.task("usemin", ["deleteDistFolder"], function() {
@@ -36,7 +36,7 @@ gulp.task("usemin", ["deleteDistFolder"], function() {
       css: [function() {return rev()}, function() {return cssnano()}],
       js: [function() {return rev()}, function() {return uglify()}]
     }))
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
 gulp.task("build", ["deleteDistFolder", "optimizeImages", "usemin"]);
